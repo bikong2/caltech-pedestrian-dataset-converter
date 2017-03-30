86dc493955f1049eeb8e54f83674f6e8b0a3b756
xihua.lxh <xihua.lxh@alibaba-inc.com>
2017-03-30 15:51:45 +0800

    modified with own app

--- scripts/convert_seqs.py
+++ scripts/convert_seqs.py
@@ -4,6 +4,9 @@
 import os
 import glob
 import cv2 as cv
+import os.path
+import fnmatch
+import shutil
 
 
 def save_img(dname, fn, i, frame):
@@ -11,17 +14,39 @@ def save_img(dname, fn, i, frame):
         out_dir, os.path.basename(dname),
         os.path.basename(fn).split('.')[0], i), frame)
 
+def open_save(filename, savepath, fn):
+    f = open(filename, 'rb')
+    string = str(f.read())
+    splitstring = "\xFF\xD8\xFF\xE0\x00\x10\x4A\x46\x49\x46"
+    strlist = string.split(splitstring)
+    f.close()
+    count = 0
+    fn_s = fn.split("/")
+    #if os.path.exists(savepath): shutil.rmtree(savepath)
+    #if not os.path.exists(savepath): os.mkdir(savepath)
+    for img in strlist:
+        filename = fn_s[-2]+"_"+fn_s[-1].split(".")[0]+"_"+str(count)+'.jpg'
+        filenamewithpath = os.path.join(savepath, filename)
+        #print filenamewithpath
+        if count > 0:
+            i = open(filenamewithpath, 'wb+')
+            i.write(splitstring)
+            i.write(img)
+            i.close()
+        count += 1
+
 out_dir = 'data/images'
 if not os.path.exists(out_dir):
     os.makedirs(out_dir)
 for dname in sorted(glob.glob('data/set*')):
     for fn in sorted(glob.glob('{}/*.seq'.format(dname))):
-        cap = cv.VideoCapture(fn)
-        i = 0
-        while True:
-            ret, frame = cap.read()
-            if not ret:
-                break
-            save_img(dname, fn, i, frame)
-            i += 1
+        #cap = cv.VideoCapture(fn)
+        #i = 0
+        #while True:
+        #    ret, frame = cap.read()
+        #    if not ret:
+        #        break
+        #    save_img(dname, fn, i, frame)
+        #    i += 1
         print(fn)
+        open_save(fn, out_dir, fn)
--- tests/test_plot_annotations.py
+++ tests/test_plot_annotations.py
@@ -16,27 +16,28 @@ if not os.path.exists(out_dir):
 
 img_fns = defaultdict(dict)
 
-for fn in sorted(glob.glob('data/images/*.png')):
+for fn in sorted(glob.glob('data/images/*.jpg')):
     set_name = re.search('(set[0-9]+)', fn).groups()[0]
     img_fns[set_name] = defaultdict(dict)
 
-for fn in sorted(glob.glob('data/images/*.png')):
+for fn in sorted(glob.glob('data/images/*.jpg')):
     set_name = re.search('(set[0-9]+)', fn).groups()[0]
     video_name = re.search('(V[0-9]+)', fn).groups()[0]
     img_fns[set_name][video_name] = []
 
-for fn in sorted(glob.glob('data/images/*.png')):
+for fn in sorted(glob.glob('data/images/*.jpg')):
     set_name = re.search('(set[0-9]+)', fn).groups()[0]
     video_name = re.search('(V[0-9]+)', fn).groups()[0]
-    n_frame = re.search('_([0-9]+)\.png', fn).groups()[0]
+    n_frame = re.search('_([0-9]+)\.jpg', fn).groups()[0]
     img_fns[set_name][video_name].append((int(n_frame), fn))
 
+cv.namedWindow("Video")
 n_objects = 0
 for set_name in sorted(img_fns.keys()):
     for video_name in sorted(img_fns[set_name].keys()):
-        wri = cv.VideoWriter(
-            'data/plots/{}_{}.avi'.format(set_name, video_name),
-            cv.VideoWriter_fourcc(*'XVID'), 30, (640, 480))
+        #wri = cv.VideoWriter(
+        #    'data/plots/{}_{}.avi'.format(set_name, video_name),
+        #    cv.VideoWriter_fourcc(*'XVID'), 30, (640, 480))
         for frame_i, fn in sorted(img_fns[set_name][video_name]):
             img = cv.imread(fn)
             if str(frame_i) in annotations[set_name][video_name]['frames']:
@@ -44,9 +45,11 @@ for set_name in sorted(img_fns.keys()):
                     video_name]['frames'][str(frame_i)]
                 for datum in data:
                     x, y, w, h = [int(v) for v in datum['pos']]
-                    cv.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 1)
+                    cv.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 3)
                     n_objects += 1
-                wri.write(img)
-        wri.release()
+                cv.imshow("Video", img)
+                cv.waitKey(5)
+                #wri.write(img)
+        #wri.release()
         print(set_name, video_name)
 print(n_objects)
